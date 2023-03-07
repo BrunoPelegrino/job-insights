5d13b4a7df6d8cef8ac93ce8c1d9d2e588ff2711
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2023-03-07 13:17:41 -0300

    feat: req 9 donematches_salary_range and filter_by_salary_range implemented. salaries.py done

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -25,47 +25,33 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError(
+                "As chaves min_salary ou max_salary estão ausentes no dici."
+                )
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        if min_salary > max_salary:
+            raise ValueError(
+                "O valor mínimo deve ser menor que o valor máximo"
+                )
+        salary = int(salary)
+        salary_range = min_salary <= salary <= max_salary
+        return salary_range
+    except Exception:
+        raise ValueError("o salário nao está dentro da faixa salarial")
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    filtered_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except ValueError:
+            continue
+    return filtered_jobs
