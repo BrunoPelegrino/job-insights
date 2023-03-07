76159a621638b9e09b994d17cd7ca96e7200e425
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2023-03-07 15:12:51 -0300

    feat: req11 done, test_brazilian_jobs implemented

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for item in result:
+        assert "title" in item
+        assert "salary" in item
+        assert "type" in item
